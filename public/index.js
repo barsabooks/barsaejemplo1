946fda6c9c6b306293d6932a13d166587f651063
barsabooks <111487321+barsabooks@users.noreply.github.com>
2022-08-18 04:26:46 -0300

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,1 +1,26 @@
-console.log('Hello world from client-side JS!')
+const express = require('express');
+const hbs = require("express-handlebars");
+const router = express.Router();
+const path = require("path");
+const app = express();
+
+
+
+const routeindex = require("./routes/conexiones");
+
+
+app.engine(".hbs", hbs.engine({extname: "hbs"}))
+app.set("view engine", "hbs")
+app.set("views", "./views")
+
+
+app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static('.'));
+
+
+app.use("/", routeindex);
+
+app.set("views", path.join(__dirname, "views"));
+
+
